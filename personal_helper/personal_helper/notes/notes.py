440f292d17d65a1d2264d1b405ee1d6eac45a2a5
Yakiv2023 <127395747+Yakiv2023@users.noreply.github.com>
2023-07-07 14:06:45 +0300

    Update notes.py
    скрипт має дві опції (меню): 1. додати нотатку 2. вийти. Після виходу нотатки зберігаються у файлі notes.txt

--- personal_helper/personal_helper/notes/notes.py
+++ personal_helper/personal_helper/notes/notes.py
@@ -1,1 +1,29 @@
+notes = []
 
+while True:
+    print('Меню:')
+    print('1. Додати нотатку')
+    print('2. Вийти')
+
+    choice = input('Введіть номер опції: ')
+
+    if choice == '1':
+        # Додавання нової нотатки
+        title = input('Введіть заголовок нотатки: ')
+        content = input('Введіть текст нотатки: ')
+        note = {'title': title, 'content': content}
+        notes.append(note)
+        print('Нотатка успішно додана!')
+
+    elif choice == '2':
+        # Збереження нотаток у файл
+        with open('notes.txt', 'w') as file:
+            for note in notes:
+                file.write(f"Заголовок: {note['title']}\n")
+                file.write(f"Текст: {note['content']}\n")
+                file.write('\n')
+        print('Нотатки збережені у файлі notes.txt')
+        break
+
+    else:
+        print('Невірний ввід. Будь ласка, спробуйте ще раз.')
