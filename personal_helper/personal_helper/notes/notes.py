d7e73d6d4c76288c9c4ae1c200a31681209a69d3
KatyaT27 <turuntseva21@gmail.com>
2023-07-09 00:40:22 +0100

    Changes to be committed:  "origin/feature/Було додаданий пошук за двома по title, а й по text, і так само повертав title та text параметрами, та оновлений notes.py"

--- personal_helper/personal_helper/notes/notes.py
+++ personal_helper/personal_helper/notes/notes.py
@@ -1,29 +1,91 @@
+def add_note(notes):
+    # Додавання та збереження нотатки
+    title = input('Enter a title for the note: ')
+    content = input('Enter the note text: ')
+    note = {'title': title, 'content': content}
+    notes.append(note)
+    with open('notes.txt', 'a') as file:
+        file.write(f"Title: {note['title']}\n")
+        file.write(f"Text: {note['content']}\n")
+        file.write('\n')
+    print('Note successfully added and saved!')
+    return notes
+
+
+def edit_note(notes):
+    # Редагування нотатки
+    title = input('Enter the title of the note to edit: ')
+    note_found = False
+
+    for note in notes:
+        if note['title'] == title:
+            new_title = input('Enter a new title for the note: ')
+            new_content = input('Enter new note text: ')
+            note['title'] = new_title
+            note['content'] = new_content
+            note_found = True
+            break
+
+    if note_found:
+        with open('notes.txt', 'w') as file:
+            for note in notes:
+                file.write(f"Title: {note['title']}\n")
+                file.write(f"Text: {note['content']}\n")
+                file.write('\n')
+        print('Note successfully edited and saved!')
+    else:
+        print('No note with this title was found.')
+
+
+def delete_note(notes):
+    # Видалення нотатки
+    title = input('Enter the title of the note to delete: ')
+    note_found = False
+
+    for note in notes:
+        if note['title'] == title:
+            notes.remove(note)
+            note_found = True
+            break
+
+    if note_found:
+        with open('notes.txt', 'w') as file:
+            for note in notes:
+                file.write(f"Title: {note['title']}\n")
+                file.write(f"Text: {note['content']}\n")
+                file.write('\n')
+        print('Note successfully deleted!')
+    else:
+        print('No note with this title was found.')
+
+# def search_notes(notes): - Це має бути першим
+
+
 def main():
     notes = []
 
     while True:
         print('Menu:')
         print('1. Add a note')
-        print('2. Go out')
+        print('2. Edit note')
+        print('3. Delete note')
+        print('4. Search notes')
+        print('5. Go out')
 
         choice = input('Enter the option number: ')
 
         if choice == '1':
-        # Додавання та збереження нотатки
-            title = input('Enter a title for the note: ')
-            content = input('Enter the note text: ')
-            note = {'title': title, 'content': content}
-            notes.append(note)
-            with open('notes.txt', 'a') as file:
-                file.write(f"Title: {note['title']}\n")
-                file.write(f"Text: {note['content']}\n")
-                file.write('\n')
-            print('Note successfully added and saved!')
-
+            notes = add_note(notes)
         elif choice == '2':
-            # Вихід з програми
-            break
+            edit_note(notes)
+        elif choice == '3':
+            delete_note(notes)
 
+        #elif choice == '4':
+            #search_notes(notes)
+            
+        elif choice == '5':
+            break
         else:
             print('Invalid input. Please try again.')
 
--- personal_helper/personal_helper/notes/search_notes.py
+++ personal_helper/personal_helper/notes/search_notes.py
@@ -1,73 +1,19 @@
 """
-The code now includes a new feature to search for notes based on a keyword. 
-When the user selects the option to search notes, they can enter a keyword, 
-and the program will find and display the notes containing the keyword in their titles. 
-The search is case-insensitive, and the matching notes' titles and contents are printed on the screen. 
+The code now includes a new feature to search for notes based on a keyword.
+When the user selects the option to search notes, they can enter a keyword,
+and the program will find and display the notes containing the keyword in their titles or contents.
+The search is case-insensitive, meaning it will match regardless of the letter casing.
+The matching notes' titles and contents are then printed on the screen.
 If no matching notes are found, a "No notes found" message is displayed.
 """
 
-"""
-Але в notes.py потрібно буде додати: 
-
-        elif choice == '2':
-            # Пошук нотаток
-            search_notes()
-
-        elif choice == '3':
-            # Вихід з програми
-            break
-
-        else:
-            print('Invalid input. Please try again.')
-
-
-"""
-
-"""
-Або відразу notes.py потрібно буде додати: 
-
-        elif choice == '2':
-            # Пошук нотаток
-            search_query = input('Enter a search query: ')
-            search_results = search_notes(notes, search_query)
-            if search_results:
-                print('Search Results:')
-                for note in search_results:
-                    print(f"Title: {note['title']}")
-                    print(f"Text: {note['content']}")
-                    print('---')
-            else:
-                print('No matching notes found.')
-
-        elif choice == '3':
-            # Вихід з програми
-            break
-
-        else:
-            print('Invalid input. Please try again.')
-
-
-if __name__ == "__main__":
-    main()
-
-"""
-
-def search_notes():
+def search_notes(notes):
     keyword = input('Enter a keyword to search: ')
     found_notes = []
 
-    with open('notes.txt', 'r') as file:
-        lines = file.readlines()
-        i = 0
-        while i < len(lines):
-            if lines[i].startswith('Title:') and keyword.lower() in lines[i].lower():
-                title = lines[i][7:].strip()
-                content = lines[i+1][6:].strip()
-                note = {'title': title, 'content': content}
-                found_notes.append(note)
-                i += 2
-            else:
-                i += 1
+    for note in notes:
+        if keyword.lower() in note['title'].lower() or keyword.lower() in note['content'].lower():
+            found_notes.append(note)
 
     if found_notes:
         print('Found notes:')
@@ -80,5 +26,9 @@ def search_notes():
 
 
 if __name__ == "__main__":
-    search_notes()
+    """
+    Here should be your code that creates a list of notes and 
+    passes it to the search_notes() function
+    '"""
+   pass
 
